2d8e307714f6aa839cc26d2aa2f4c042176b45bb
nabeelmehmood <nabeel_mehmood@hotmail.com>
2022-12-30 16:48:46 +0500

    add loading screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Products from './pages/Products'
-import Cart from './pages/Cart'
-import Nav from './components/Nav'
-import ProductDescription from './pages/ProductDescription'
+import { Routes, Route } from 'react-router-dom';
+import gsap from 'gsap';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import Nav from './components/Nav';
+import ProductDescription from './pages/ProductDescription';
+
+window.addEventListener('load', () => {
+  gsap
+    .timeline()
+    .to('.loading-screen .ring', { autoAlpha: 0 })
+    .to('.loading-screen', { clipPath: 'circle(0% at 0% 100%)', duration: 1, delay: -0.5 });
+});
+
 function App() {
   return (
     <>
-    <Nav/>
-    <Routes>
-      <Route path='/' element={<Home h='2'/>}/>
-      <Route path='/products' element={<Products/>}/>
-      <Route path='/cart' element={<Cart/>}/>
-      <Route path='/products/:id' element={<ProductDescription/>} />
-    </Routes>
-  
-
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home h="2" />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products/:id" element={<ProductDescription />} />
+      </Routes>
     </>
-
-  )
+  );
 }
 
 export default App;
