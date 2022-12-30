3426e6ac7aa9fa007a5e0040b95d081d502277ba
mirthepeer <mirthepeer@gmail.com>
2022-12-30 04:56:04 -0500

    added  about page route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <>
     <Nav/>
     <Routes>
-      <Route path='/' element={<Home h='2'/>}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/products' element={<Products/>}/>
       <Route path='/cart' element={<Cart/>}/>
       <Route path='/products/:id' element={<ProductDescription/>} />
