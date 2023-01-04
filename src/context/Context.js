9d9cbe5ea9aa879c5da9971644f70ac0e2750f8c
nabeelmehmood <nabeel_mehmood@hotmail.com>
2023-01-04 13:47:44 +0500

    fix context update for categories

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,46 +1,63 @@
-import {createContext, useEffect, useState} from 'react'
+import { createContext, useEffect, useState } from 'react';
 
-const Context = createContext()
+const Context = createContext();
 
-function ContextProvider({children}){
+function ContextProvider({ children }) {
+  const [cart, setCart] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [categoryProducts, setCategoryProducts] = useState({});
 
-    const [cart, setCart] = useState([])
-    const [categories, setCategories] = useState([])
-    const [products, setProducts] = useState([])
-    useEffect(()=>{
-        fetch('https://fakestoreapi.com/products')
-        .then(res=>res.json())
-        .then(data=> setProducts(data))
-    },[])
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
 
-    useEffect(()=>{
-        fetch('https://fakestoreapi.com/products/categories')
-        .then(res=>res.json())
-        .then(data=> setCategories(data))
-    },[])
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/categories')
+      .then(res => res.json())
+      .then(data => setCategories(data));
+  }, []);
 
-    function handleAdd(newItem){
-        console.log(cart);
-        setCart(prev=>[...prev, newItem])
-    }
+  function handleAdd(newItem) {
+    console.log(cart);
+    setCart(prev => [...prev, newItem]);
+  }
 
-    function removeItem(id){
-        setCart(prev=> prev.filter(prod=>prod.id!==id))
-    }
+  function removeItem(id) {
+    setCart(prev => prev.filter(prod => prod.id !== id));
+  }
 
-    function emptyCart(){
-        setCart([])
-    }
-    
+  function emptyCart() {
+    setCart([]);
+  }
 
-    
+  const getCategoryProducts = async (category = '') => {
+    if (categoryProducts[category]) return;
 
+    const prodRes = await fetch(`https://fakestoreapi.com/products/category/${category}`)
+    const result = await prodRes.json();
+    setCategoryProducts({...categoryProducts, [category]: result});
+  };
 
-    return (
-        <Context.Provider value={{cart, products, handleAdd, removeItem, emptyCart, categories,setProducts}}>
-            {children}
-        </Context.Provider>
-    )
+  return (
+    <Context.Provider
+      value={{
+        cart,
+        products,
+        handleAdd,
+        removeItem,
+        emptyCart,
+        categories,
+        setProducts,
+        categoryProducts,
+        getCategoryProducts
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 }
 
-export {Context, ContextProvider}
+export { Context, ContextProvider };
--- src/pages/CategoryItems.js
+++ src/pages/CategoryItems.js
@@ -1,41 +1,39 @@
-import { Context } from "../context/Context"
-import { useContext, useEffect } from "react"
-import { useParams } from "react-router-dom"
-import Product from "../components/Product"
+import { Context } from '../context/Context';
+import { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Product from '../components/Product';
 
-export default function CategoryItems(){
-    const {category} = useParams()
-    const  {setProducts, products, handleAdd, cart, removeItem} = useContext(Context)
-    
-    useEffect(()=>{
-        fetch(`https://fakestoreapi.com/products/category/${category}`)
-        .then(res=>res.json())
-        .then(data=>setProducts(data))
-    },[])
+export default function CategoryItems() {
+  const { category } = useParams();
+  const { handleAdd, cart, removeItem, getCategoryProducts, categoryProducts } = useContext(Context);
 
-    const catagoryItemsList = products.map(item=>{
-        return (
-            <Product key={item.id}
-             item={item}
-              handleAdd={handleAdd}
-               cart={cart}
-               removeItem={removeItem}
-            />
+  useEffect(() => {
+    getCategoryProducts(category);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-        )
-    })
+  const catagoryItemsList =
+    categoryProducts[category] &&
+    categoryProducts[category].map(item => {
+      return (
+        <Product
+          key={item.id}
+          item={item}
+          handleAdd={handleAdd}
+          cart={cart}
+          removeItem={removeItem}
+        />
+      );
+    });
 
- 
-
-    
-    return (
-        <>
-        <div className="current-tab">
-            <h1>Shop <span className="category-item">{category}</span></h1>
-        </div>
-        <div className="container">
-            {catagoryItemsList}
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="current-tab">
+        <h1>
+          Shop <span className="category-item">{category}</span>
+        </h1>
+      </div>
+      <div className="container">{catagoryItemsList}</div>
+    </>
+  );
 }
