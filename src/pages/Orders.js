fd1b218151b7ddadc384c10a5be899a7ca02414c
mirthepeer <mirthepeer@gmail.com>
2023-01-16 03:28:35 -0500

    User is now notified when they have no order history on the orders page

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -17,9 +17,11 @@ export default function Orders(){
         )
     })
     
+    const noOrdersMessage = <p className="center accent">No Orders Found</p>
     return (
         <div className="current-tab">
         <h1 className="center accent">Past Orders</h1>
+        {myOrders.length>0 ? '' : noOrdersMessage }
         <div>
             <div className="orders-list">
             {myOrders}
